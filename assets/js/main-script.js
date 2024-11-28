7bab4bf98ced0dda87a0f1b501d9224f6b0b1ae2
Ashikur Rahman <proashik012@gmail.com>
2024-11-28 07:42:32 +0600

    style updated

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -1,9 +1,18 @@
-$('document').ready(function(){
+$(document).ready(function () {
+    // Initially show the first tab content
+    $(".catalog-result > div").first().addClass("active");
+    $(".catalog-category li").first().addClass("active");
 
-    
+    $(".catalog-category li").on("click", function (e) {
+        e.preventDefault();
 
-    $('.tabs-demo').jQueryTab({
-        // options here
+        // Remove 'active' class from all items
+        $(".catalog-category li").removeClass("active");
+        $(".catalog-result > div").removeClass("active");
+
+        // Add 'active' class to the clicked tab and corresponding content
+        $(this).addClass("active");
+        const target = $(this).data("target");
+        $(target).addClass("active");
     });
-
-})
+});
